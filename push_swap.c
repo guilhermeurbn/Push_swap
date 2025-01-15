6c520425a37eb427228e41fc1ea3c1ec520d7594
Guilherme Urbano <guilhermeurbano@MacBook-Pro-de-Guilherme.local>
2025-01-15 19:25:45 +0000

    perdi 10 anos de vida

--- push_swap.c
+++ push_swap.c
@@ -6,35 +6,130 @@
 /*   By: guisanto <guisanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 16:21:09 by guisanto          #+#    #+#             */
-/*   Updated: 2025/01/12 20:38:59 by guisanto         ###   ########.fr       */
+/*   Updated: 2025/01/15 19:19:46 by guisanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-typedef struct s_stack_node
+void print_stack(int *stack, int n)
 {
-    int                 value;
-    int                 current_position;
-    int                 final_index;
-    int                 push_price;
-    bool                above_median;
-    bool                cheapest;
-    struct s_stack_node *target_node;
-    struct s_stack_node *next;
-    struct s_stack_node *prev;
-}               t_stack_node;   
+    int i = 0;
 
-int main(int argc, char ** argv)
+    while (i < n)
+    {
+        printf("%d ", stack[i]); // Exibe cada elemento separado por espaço
+        i++;
+    }
+    printf("\n"); // Nova linha ao final
+}
+//vai transformar os argumentos em interios
+int	ft_atoi(const char *str)
 {
-    t_stack_node    *a;
-    t_stack_node    *b;
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	if (str[i] == '-' || str[i] == '+')
+		return (0);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+//busca o número máximo
+int ft_max(int *stack, int n)
+{
+    int i;
+    int max;
+
+    i = 0;
+    max = stack[0];
+    while(i < n)
+    {
+        if (stack[i] > max)
+            max = stack[i];
+        i++;
+    }
+    return (max);
+}
+
+//checa se tem números duplicados
+int check_duplicates(int *stack, int n)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while(i < n)
+    {
+        j = i + 1;
+        while(j < n)
+        {
+            if(stack[i] == stack[j])
+                return (0);
+            j++;
+        }
+        i++;
+    }
+    return (1);
+}
+void push_swap(int *stack, int n)
+{
+    int max;
+    int exp;
     
-    a = NULL;
-    b = NULL;
-    if (argc == 1 || (argc == 2 && !argv[1][0]))
+    max = 0;
+    exp = 1;
+    max = ft_max(stack, n);
+    
+    while(max / exp > 0)
+    {
+        count_sort(stack, n, exp);
+        exp *= 10;
+    }
+    if (check_duplicates(stack, n) == 0)
+    {
+        printf("error!\nNúmeros duplicados!\n");
+        return ;
+    }
+    print_stack(stack, n);
+}
+int main(int ac, char **av)
+{
+    int *stack;
+    int n = ac - 1;
+    int i = 0;
+    
+    if (ac < 2)
+    {
+        printf("error!\n");
         return (1);
-    else if (argc == 2)
-        argv = ft_split(argv[1], ' ');
+    }
     
+    stack = malloc(n * sizeof(int));
+    if (!stack)
+        return (1);
+    while(i < ac - 1)
+    {
+        stack[i] = ft_atoi(av[i + 1]);
+        i++;
+    }
+    push_swap(stack, n);
+    free(stack);
+    return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,18 +6,25 @@
 /*   By: guisanto <guisanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 17:02:18 by guisanto          #+#    #+#             */
-/*   Updated: 2025/01/12 20:37:17 by guisanto         ###   ########.fr       */
+/*   Updated: 2025/01/15 18:36:50 by guisanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+#  define PUSH_SWAP_H
 
 #include <stdbool.h>
+#include <stdlib.h>
 #include <limits.h>
-#include "libft/libft.h"
-#include "ft_printf/ft_printf.h"
-
 #include <stdio.h>
+#include <unistd.h>
+
+void    count_sort(int *stack, int n, int exp);
+void    print_stack(int *stack, int n);
+void    push_swap(int *stack, int n);
+int     ft_atoi(const char *str);
+int     check_duplicates(int *stack, int n);
+int     ft_max(int *stack, int n);
+
 
 #endif
